8488517a6113e767cc97187b5c668f5311b0433c
Saketh <23B0987@iitb.ac.in>
2026-02-03 23:10:18 +0530

    student dashboard

--- app.js
+++ app.js
@@ -137,18 +137,19 @@ app.get('/logout', (req, res) => {
 app.get('/student/dashboard', isAuthenticated, async (req, res) => {
     const userId = req.session.user.user_id;
 
-
+    const name_query = "SELECT full_name FROM users WHERE user_id = $1";
     const reg_courses_query = "SELECT courses.* FROM courses JOIN registrations ON courses.course_id = registrations.course_id WHERE registrations.student_id = $1";
     const available_courses_query = "SELECT * FROM courses WHERE course_id NOT IN (SELECT course_id FROM registrations WHERE student_id = $1)";
     const total_credits_query = "SELECT SUM(c.credits) AS total_credits FROM courses c JOIN registrations r ON c.course_id = r.course_id WHERE r.student_id = $1";
 
+    const name = (await pool.query(name_query, [userId])).rows[0].full_name;
     const registered_courses = (await pool.query(reg_courses_query, [userId])).rows;
     const available_courses = (await pool.query(available_courses_query, [userId])).rows;
     const total_credits_result = (await pool.query(total_credits_query, [userId])).rows;//can be empty
     const total_credits = total_credits_result[0].total_credits || 0;
 
     res.render('student_dashboard', {
-        user: req.session.user,
+        user: { ...req.session.user, full_name: name },
         registered_courses: registered_courses,
         available_courses: available_courses,
         total_credits: total_credits
@@ -162,19 +163,67 @@ app.get('/student/dashboard', isAuthenticated, async (req, res) => {
 // 4. Check Course Capacity (Optional)
 // 5. Insert into Registrations table
 app.post('/student/register', isAuthenticated, async (req, res) => {
+    const userId = req.session.user.user_id;
+    const courseId = req.body.course_id;
 
+    //1. Check if course exists
+    const course_check_query = "SELECT * FROM courses WHERE course_id = $1";
+    const course_check_result = await pool.query(course_check_query, [courseId]);
+    if (course_check_result.rows.length === 0) {
+        return res.status(400).send("Course does not exist.");
+    }
+
+    const slot_credits_query = "SELECT slot, credits FROM courses WHERE course_id = $1";
+    const slot = (await pool.query(slot_credits_query, [courseId])).rows[0].slot;
+    const credits = Number((await pool.query(slot_credits_query, [courseId])).rows[0].credits);
+
+    //2. Check for Slot Clash (Cannot register for same slot twice)
+    const slots_taken_query = "SELECT c.slot FROM courses c JOIN registrations r ON c.course_id = r.course_id WHERE r.student_id = $1";
+    const slots_taken = await pool.query(slots_taken_query, [userId]);
+    if(slots_taken.rows.some(row => row.slot === slot)) {
+        return res.status(400).send("Can't take this course, slot clash detected.");
+    }
+
+    //3. Check Credit Limit (Max 24 credits)
+    const total_credits_query = "SELECT SUM(courses.credits) AS total_credits FROM courses JOIN registrations ON courses.course_id = registrations.course_id WHERE registrations.student_id = $1";
+    const total_credits_result = await pool.query(total_credits_query, [userId]);//can be empty
+    const total_credits = Number(total_credits_result.rows[0].total_credits) || 0;
+    if(total_credits + credits > 24) {
+        return res.status(400).send("Can't take this course, credit limit exceeded.");
+    }
+
+    //5. Insert into Registrations table
+    const insert_registration_query = "INSERT INTO registrations (student_id, course_id) VALUES ($1, $2)";
+    await pool.query(insert_registration_query, [userId, courseId]);
+
+    res.redirect('/student/dashboard');
 });
 
 // TODO: Implement drop logic
 // 1. Delete from Registrations table
 app.post('/student/drop', isAuthenticated, async (req, res) => {
+    const userId = req.session.user.user_id;
+    const courseId = req.body.course_id;
 
+    const delete_registration_query = "DELETE FROM registrations WHERE student_id = $1 AND course_id = $2";
+    await pool.query(delete_registration_query, [userId, courseId]);
+
+    res.redirect('/student/dashboard');
 });
 
 
 // TODO: Render instructor dashboard
 // 1. Fetch courses taught by this instructor
 app.get('/instructor/dashboard', isAuthenticated, isInstructor, async (req, res) => {
+    const userID = req.session.user.user_id;
+
+    const courses_teaching_query = "SELECT * FROM courses WHERE instructor_id = $1";
+    const courses_teaching = (await pool.query(courses_teaching_query, [userID])).rows;
+
+    res.render('instructor_dashboard', {
+        user: req.session.user,
+        courses: courses_teaching
+    });
 
 });
 
